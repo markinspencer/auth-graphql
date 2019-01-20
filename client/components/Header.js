b241f652a0f7e697f06a7fdbe3fe3a7d1c35dd9c
Spencer Markin <markinspencer@gmail.com>
2019-01-20 01:17:32 -0500

    build out the header component and wire up logout

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,53 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import currentUserQuery from "../queries/currentUser";
+import logoutMutation from "../mutiations/logout";
 
 class Header extends Component {
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query: currentUserQuery }]
+    });
+  }
+
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) return <div />;
+
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
+    }
+
+    return (
+      <div>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </div>
+    );
+  }
+
   render() {
-    return <div> Header </div>;
+    return (
+      <nav>
+        <Link to="/" className="brand-logo left">
+          Home
+        </Link>
+        <div className="nav-wrapper">
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+export default graphql(logoutMutation)(graphql(currentUserQuery)(Header));
